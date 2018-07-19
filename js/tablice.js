eb23f5d22bbd200603cd7f3847154e2fe55ff962
charlienas <charlie.n003@gmail.com>
2018-07-19 16:04:22 +0200

    tablice.jss ztworzenie i zmiany

--- js/tablice.js
+++ js/tablice.js
@@ -16,3 +16,41 @@ console.log(zmienna);
 //Dodawanie wartosci na poczatek tablicy
 zmienna.unshift('Monika');
 console.log(zmienna);
+
+//Usuwanie elementow
+//metoda pop() usuwa element z konca tablicy
+zmienna.pop();
+console.log(zmienna);
+
+//metoda shift() usuwa pierwszy element z tablicy
+zmienna.shift();
+console.log(zmienna);
+//console.log(zmienna.length);
+//oddaje numer elementow
+
+//join() - zwraca wszystkie elementy talicy wypisane jako tekst, w nawiasach opcjonalnie mozemy podac spearator, domyslnie ','
+console.log(zmienna.join(' '));
+
+//reverse() sortuje tablice - niezaleznie od tego, gdzie zostal
+console.log(zmienna.reverse());
+console.log(zmienna);
+
+//sort() sortuje tablice - niezaleznie od tego, gdzie zostal wykonany
+console.log(zmienna.sort());
+console.log(zmienna);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
