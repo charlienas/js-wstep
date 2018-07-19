5ad76f8c5a50de74d2dd05e3284f7492d386abad
charlienas <charlie.n003@gmail.com>
2018-07-19 13:51:11 +0200

    variables and constants

---
+++ js/funkcje.js
@@ -0,0 +1,17 @@
+//function wypiszDoKonsli(numer1, numer2) {
+//    console.log('123');
+//    
+//}
+//
+//wypiszDoKonsli(3, 5);
+
+//wyrazenie funkcyjne 
+const wppiszDoKonsoli = function() {
+    console.log('123');
+}
+
+wppiszDoKonsoli();
+
+
+
+
---
+++ js/main.js
@@ -0,0 +1,22 @@
+//komantarze ctrl + / lub cmd + / - komentarz liniowy 
+
+
+/*
+//mozna laczyc komentarz liniowy z blokowym 
+*/
+
+//deklaracja zmiennej, to zarezerwowanie komorki pamieia dla jakiejs wartosci
+//let zmienna;
+
+
+//inicikalizacja zmiennej to przypisanie wartosci do nazwangeo miejsca w pamieci
+let liczba = 10;
+
+//stale - zmienne, ktorych nie da sie zmieniac (const)
+const numberOfMonths = 12;
+
+//numberOfMonths = 10; - ie mozna zmieniac wartosci stalych 
+console.log('123');
+
+
+
