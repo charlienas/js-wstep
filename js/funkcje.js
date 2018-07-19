0ae02a99e0f944c7ce88f36b80d8a9de208fb518
charlienas <charlie.n003@gmail.com>
2018-07-19 14:04:26 +0200

    funkcje z parametrami

--- js/funkcje.js
+++ js/funkcje.js
@@ -6,12 +6,16 @@
 //wypiszDoKonsli(3, 5);
 
 //wyrazenie funkcyjne 
-const wppiszDoKonsoli = function() {
+const wppiszDoKonsoli = function () {
     console.log('123');
 }
 
 wppiszDoKonsoli();
 
 
+//funkcje parametrowe
+function dodawanie(parametr1, parametr2) {
+    console.log(parametr1 + parametr2);
+}
 
-
+dodawanie(1, '2');
